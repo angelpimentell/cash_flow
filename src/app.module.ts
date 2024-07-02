9704fe810961573f3d0c23998009784a92b9587f
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-07-02 12:21:09 -0400

    Add export for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './Finance/account/account.module';
 import { TransactionModule } from './Finance/transaction/transaction.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AccountModule, TransactionModule],
+  imports: [
+    AccountModule,
+    TransactionModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
