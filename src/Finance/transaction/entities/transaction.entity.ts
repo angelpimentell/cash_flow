5fc7da2302999d606661267e77d06d066684a739
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-07-02 11:17:44 -0400

    Add transaction attributes

--- src/Finance/transaction/entities/transaction.entity.ts
+++ src/Finance/transaction/entities/transaction.entity.ts
@@ -1,1 +1,16 @@
-export class Transaction {}
+import { Account } from 'src/Finance/account/entities/account.entity';
+import { Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  amount: number;
+
+  @ManyToOne(() => Account)
+  account_source_id: Account;
+
+  @ManyToOne(() => Account)
+  account_destination_id: string;
+}
