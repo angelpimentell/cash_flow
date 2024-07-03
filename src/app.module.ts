74e0636a1ca4d43574ccf73b9a5bd0acfe4b8ce1
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-07-03 07:58:29 -0400

    Add migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         password: configService.get('DATABASE_PASSWORD'),
         database: configService.get('DATABASE_DATABASE'),
         entities: [],
+        migrations: ['dist/migrations/*.ts'],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
         synchronize: true,
       }),
     }),
---
+++ src/migrations/1720007757967-CreateTransactionTable.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTransactionTable1720007757967 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
