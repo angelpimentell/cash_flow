e65f5fc9acc5bb7ce3ff44ac722254c014d30abb
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-07-03 13:57:15 -0400

    Change typeorm config

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -7,12 +7,12 @@ config();
 const configService = new ConfigService();
 
 export default new DataSource({
-  type: 'postgres',
-  host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT'),
-  username: configService.get('DB_USER'),
-  password: configService.get('DB_PASSWORD'),
-  database: configService.get('DB_NAME'),
+  type: 'mysql',
+  host: configService.get('DATABASE_HOST'),
+  port: configService.get('DATABASE_PORT'),
+  username: configService.get('DATABASE_USERNAME'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
   entities: [`${__dirname}/../src/**/*.entity{.ts,.js}`],
   synchronize: configService.get('nodenv') === 'development',
   logging: configService.get('nodenv') === 'development',
