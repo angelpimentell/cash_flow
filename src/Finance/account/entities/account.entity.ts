3b91aad5fe9a6172faea27abce6fcb896bef4246
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-07-04 14:52:55 -0400

    Change account entity

--- src/Finance/account/entities/account.entity.ts
+++ src/Finance/account/entities/account.entity.ts
@@ -1,24 +1,33 @@
 import { DEFAULT_DECIMAL, DEFAULT_INTEGER } from 'src/constants';
 import { Transaction } from 'src/Finance/transaction/entities/transaction.entity';
 import { User } from 'src/user/entities/user.entity';
-import { Column, PrimaryGeneratedColumn, Entity, ManyToOne, OneToMany } from 'typeorm';
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
 
 @Entity('accounts')
 export class Account {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  user_id: number;
+  @ManyToOne(() => User, (user) => user.accounts)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 
   @Column()
   name: string;
 
-  @Column('decimal', { precision: DEFAULT_INTEGER, scale: DEFAULT_DECIMAL })
+  @Column('decimal', {
+    precision: DEFAULT_INTEGER,
+    scale: DEFAULT_DECIMAL,
+    default: '0.00',
+  })
   balance: number;
-
-  @ManyToOne(() => User, (user) => user.accounts)
-  user: User;
 
   @OneToMany(() => Transaction, (transaction) => transaction.account)
   transactions: Transaction[];
