f3fe901659f360ae4e78b0a03d13e6c0b27c3d1b
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-07-04 09:50:12 -0400

    Add users migrations

--- src/User/entities/user.entity.ts
+++ src/User/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+}
