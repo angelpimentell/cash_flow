379179490cc8f0b8ae7aab32672adbfdc14defc7
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-07-05 11:45:02 -0400

    Remove attributes for UpdateUserDTO

--- src/User/dto/update-user.dto.ts
+++ src/User/dto/update-user.dto.ts
@@ -1,17 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @IsOptional()
-  @IsString()
-  name?: string;
-
-  @IsOptional()
-  @IsString()
-  username?: string;
-
-  @IsOptional()
-  @IsString()
-  password?: string;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
